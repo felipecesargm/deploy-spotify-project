735991c7e19d0d4a5ea4d625c289d5381e2da391
felipecesargm <felipe.cesar1997@gmail.com>
2025-02-17 00:30:23 -0300

    Corrigindo script de build

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -8,16 +11,22 @@ const PORT = 3001;
 
 app.use(cors()); // Essa linha é necessária para que o front-end consiga acessar o back-end, pois ela libera o acesso externo à API.
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
     response.send("Hello!");
 });
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
     response.send(await db.collection("artists").find({}).toArray());
 });
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
     response.send(await db.collection("songs").find({}).toArray());
 });
 
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("*", async (request, response) => {
+    response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
+});
+
 app.listen(PORT, () => {
-    console.log("Server is running on port 3001");
+    console.log(`The server is listenin in port ${PORT}`);
 });
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+import "dotenv/config";
 
-const URL = "http://localhost:3001";
+const { NODE_ENV } = process.env;
+
+const URL = NODE_ENV === "development" ? "http://localhost:300/api" : "/api";
 
 const responseArtist = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
